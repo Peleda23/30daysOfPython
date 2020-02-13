e158e0f5f0fef5033b82788ba1a86b6ef2ff77d3
Peleda23 <kosmosas23@gmail.com>
2020-02-13 21:39:33 +0200

    Did some build in functions, worked with variables

---
+++ day_2/variable.py
@@ -0,0 +1,67 @@
+#Inside 30DaysOfPython create a folder called day_2. Inside this folder create a file name called variables.py
+#Writ a python comment saying 'Day 2: 30 Days of python programming'
+# Day 2: 30 Days of python programming
+#Declare a first name variable and assign a value to it
+first_name = 'Tomas'
+#Declare a last name variable and assign a value to it
+last_name = 'Tomukas'
+#Declare a full name variable and assign a value to it
+full_name = 'Tomas Tomukas'
+#Declare a country variable and assign a value to it
+country = 'Lithuania'
+#Declare a city variable and assign a value to it
+city = 'Klaiped'
+#Declare an age variable and assign a value to it
+age = 33
+#Declare a year variable and assign a value to it
+year = 2020
+#Declare a variable is_married and assign a value to it
+is_married = True
+#Declare a variable is_true and assign a value to it
+is_true = True
+#Declare a variable is_light_on and assign a value to it
+is_light_on = 'yes'
+#Declare multiple variable on one line
+name, lastname, work, wife, number = 'Tadas', 'Murinas', 'No', True, 23
+#Check the data type of all your variables using type() built in function
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(is_married))
+#Using the len() built-in function find the length of your first name
+print(len(first_name))
+#Compare the length of your first name and your last name
+print('First name: ', len(first_name))
+print('Last name: ', len(last_name))
+#Declare 5 as num_one and 4 as num_two
+num_one = 5
+num_two = 4
+#Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
+print(total)
+#Subtract num_two from num_one and assign the value to a variable diff
+diff = num_one - num_two
+print(diff)
+#Multiply num_two and num_one and assign the value to a variable product
+product = num_two * num_two
+print(product)
+#Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
+print(division)
+#Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two % num_one
+print(remainder)
+#Calculate num_one the power of num_two and assign the value to a variable exp
+exp = num_one ** num_two
+print(exp)
+#Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two
+print(floor_division)
+#The radius of a circle is 30 meters.
+#Calculate the area of a circle and assign the value to a variable area_of_circle
+        #Calculate the circumference of a circle and assign the value to a variable circum_of_circle
+        #Take radius as user input and calculate the area.
+#Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+vardas = input('Koks tavo vardas: ')
+pavarde = input('Kokia pavarde: ')
+    #Run help('keywords') on python shell or in your file check the reserved words
